9f429ada451ee246b6ed3241f92bbeac66d4c72a
rafadlis <rahmatfadlis@gmail.com>
2025-06-05 10:37:06 +0700

    feat(theme): update next-themes to version 0.4.6 and integrate ThemeProvider in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import Providers from "@/lib/providers";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,13 +12,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
-        <Providers>
-          {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Providers>
+            {children}
 
-          <Toaster closeButton />
-        </Providers>
+            <Toaster closeButton />
+          </Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
